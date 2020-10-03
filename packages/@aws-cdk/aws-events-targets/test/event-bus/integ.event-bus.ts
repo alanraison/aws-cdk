05f9d5d5ccef9eac9e634597645ed57747cc8b99
alanraison <alan.raison+github@gmail.com>
2020-10-03 16:02:32 +0000

    Integration test

--- packages/@aws-cdk/aws-events-targets/test/event-bus/integ.event-bus.ts
+++ packages/@aws-cdk/aws-events-targets/test/event-bus/integ.event-bus.ts
@@ -1,13 +1,22 @@
 /// !cdk-integ pragma:ignore-assets
+import * as events from '@aws-cdk/aws-events';
 import * as cdk from '@aws-cdk/core';
+import * as targets from '../../lib';
 
 const app = new cdk.App();
 
-class EventBus extends cdk.Stack {
+class EventSourceStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression('rate(1 minute)'),
+    });
+    rule.addTarget(new targets.EventBus({
+      eventBusArn: `arn:aws:events:${this.region}:111111111111:event-bus/test-bus`,
+    }));
   }
 }
 
-new EventBus(app, 'event-id-stack');
+new EventSourceStack(app, 'event-source-stack');
 app.synth();
